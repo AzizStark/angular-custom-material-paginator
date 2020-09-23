7786c790f6475d6d0ed9f6014f08c59af8671e67
unknown <theazizstark@gmail.com>
2020-09-23 20:54:21 +0530

    Remove items

--- src/app/pagination.directive.ts
+++ src/app/pagination.directive.ts
@@ -23,9 +23,11 @@ interface PageObject {
   selector: '[appPagination]'
 })
 export class PaginationDirective {
+  private _currentPage = 1;
+  private _pageGapTxt2 = '..';
   private _pageGapTxt = '...';
-  private _rangeStart: number;
-  private _rangeEnd: number;
+  private _rangeStart;
+  private _rangeEnd;
   private _buttons = [];
   private _curPageObj: PageObject = {
     length: 0,
@@ -35,46 +37,20 @@ export class PaginationDirective {
   };
 
   @Input()
-  get showTotalPages(): number {
-    return this._showTotalPages;
-  }
+  get showTotalPages(): number { return this._showTotalPages; }
   set showTotalPages(value: number) {
     this._showTotalPages = value % 2 === 0 ? value + 1 : value;
   }
-  private _showTotalPages = 5;
-
-  get inc(): number {
-    return this._showTotalPages % 2 === 0
-      ? this.showTotalPages / 2
-      : (this.showTotalPages - 1) / 2;
-  }
-
-  get numOfPages(): number {
-    return this.matPag.getNumberOfPages();
-  }
-
-  get lastPageIndex(): number {
-    return this.matPag.getNumberOfPages() - 1;
-  }
+  private _showTotalPages = 2;
 
   constructor(
     @Host() @Self() @Optional() private readonly matPag: MatPaginator,
     private vr: ViewContainerRef,
     private ren: Renderer2
   ) {
-    //to rerender buttons on items per page change and first, last, next and prior buttons
-    this.matPag.page.subscribe((e: PageObject) => {
-      if (
-        this._curPageObj.pageSize !== e.pageSize &&
-        this._curPageObj.pageIndex !== 0
-      ) {
-        e.pageIndex = 0;
-        this._rangeStart = 0;
-        this._rangeEnd = this._showTotalPages - 1;
-      }
-      this._curPageObj = e;
-
-      this.initPageRange();
+    // Sub to rerender buttons when next page and last page is used
+    this.matPag.page.subscribe((v) => {
+      this.switchPage(v.pageIndex);
     });
   }
 
@@ -85,85 +61,94 @@ export class PaginationDirective {
     const nextPageNode = this.vr.element.nativeElement.querySelector(
       'button.mat-paginator-navigation-next'
     );
-    const prevButtonCount = this._buttons.length;
+    let prevButtonCount = this._buttons.length;
 
-    // Remove buttons before creating new ones
-    if (this._buttons.length > 0) {
+    // remove buttons before creating new ones
+    if (prevButtonCount > 0) {
       this._buttons.forEach(button => {
         this.ren.removeChild(actionContainer, button);
       });
       // Empty state array
-      this._buttons.length = 0;
+      prevButtonCount = 0;
     }
+    const pagecount = this.vr.element.nativeElement.childNodes[0].childNodes[0]
+      .childNodes[1].childNodes[0];
+    console.log(pagecount);
+    this.ren.setStyle(pagecount, 'white-space', 'nowrap');
 
     // 1initialize next page and last page buttons
-    if (this._buttons.length == 0) {
-      let nodeArray = this.vr.element.nativeElement.childNodes[0].childNodes[0]
-        .childNodes[2].childNodes;
+    if (prevButtonCount === 0) {
+      const nodeArray = this.vr.element.nativeElement.childNodes[0].childNodes[0]
+        .childNodes[1].childNodes;
+
       setTimeout(() => {
-        for (let i = 0; i < nodeArray.length; i++) {
-          if (nodeArray[i].nodeName === 'BUTTON') {
-            //Next Button styles
-            if (nodeArray[i].innerHTML.length > 100 && nodeArray[i].disabled) {
-              this.ren.setStyle(nodeArray[i], 'color', '#999999');
-              this.ren.setStyle(nodeArray[i], 'margin', '.5%');
+        for (const node of nodeArray) {
+          if (node.nodeName === 'BUTTON') {
+            // Next Button styles
+            if (node.innerHTML.length > 100 && node.disabled) {
+              this.ren.setStyle(node, 'color', '#999999');
+              this.ren.setStyle(node, 'margin', '.5%');
             } else if (
-              nodeArray[i].innerHTML.length > 100 &&
-              !nodeArray[i].disabled
+              node.innerHTML.length > 100 &&
+              !node.disabled
             ) {
-              this.ren.setStyle(nodeArray[i], 'color', '#999999');
-              this.ren.setStyle(nodeArray[i], 'margin', '.5%');
-            } else if (nodeArray[i].disabled) {
-              this.ren.setStyle(nodeArray[i], 'background-color', '#007CBE');
-              this.ren.setStyle(nodeArray[i], 'color', '#fff');
+              this.ren.setStyle(node, 'color', '#999999');
+              this.ren.setStyle(node, 'margin', '.5%');
+            } else if (node.disabled) {
+              this.ren.setStyle(node, 'background-color', '#007CBE');
+              this.ren.setStyle(node, 'color', '#fff');
             }
           }
         }
       });
     }
 
-    for (let i = 0; i < this.numOfPages; i++) {
+    let dots = false;
+    let dots2 = false;
 
-      if (i === 0) {
-        console.log(i, this._rangeStart);
-        this.ren.insertBefore(
-          actionContainer,
-          this.createButton(i, this._rangeStart),
-          nextPageNode
-        );
-      }
+    this.ren.insertBefore(
+      actionContainer,
+      this.createButton(0, this.matPag.pageIndex),
+      nextPageNode
+    );
 
-      if (i === this._rangeStart) {
-        this.ren.insertBefore(
-          actionContainer,
-          this.createButton(this._pageGapTxt, this._rangeEnd),
-          nextPageNode
-        );
-      }
+    for (let i = 1; i < this.matPag.getNumberOfPages() - 1; i = i + 1) {
 
-      if (i >= this._rangeStart + 1 && i <= this._rangeEnd - 1) {
+      if (
+        (i < this._showTotalPages && this._currentPage < this._showTotalPages && i > this._rangeStart) ||
+        (i >= this._rangeStart && i <= this._rangeEnd)
+      ) {
         this.ren.insertBefore(
           actionContainer,
           this.createButton(i, this.matPag.pageIndex),
           nextPageNode
         );
+      } else {
+        if (i > this._rangeEnd && !dots) {
+          this.ren.insertBefore(
+            actionContainer,
+            this.createButton(this._pageGapTxt, this.matPag.pageIndex),
+            nextPageNode
+          );
+          dots = true;
+        }
+        if (i < this._rangeEnd && !dots2) {
+          this.ren.insertBefore(
+            actionContainer,
+            this.createButton(this._pageGapTxt2, this.matPag.pageIndex),
+            nextPageNode
+          );
+          dots2 = true;
+        }
       }
+    }
 
-      if (i === this._rangeEnd) {
-        this.ren.insertBefore(
-          actionContainer,
-          this.createButton(this._pageGapTxt, this._rangeEnd),
-          nextPageNode
-        );
-      }
-
-      if (i + 1 === this.numOfPages) {
-        this.ren.insertBefore(
-          actionContainer,
-          this.createButton(i, this._rangeEnd),
-          nextPageNode
-        );
-      }
+    if (this.matPag.getNumberOfPages() !== 1) {
+      this.ren.insertBefore(
+        actionContainer,
+        this.createButton(this.matPag.getNumberOfPages() - 1, this.matPag.pageIndex),
+        nextPageNode
+      );
     }
   }
 
@@ -183,26 +168,20 @@ export class PaginationDirective {
     const text = this.ren.createText(pagingTxt + '');
 
     this.ren.addClass(linkBtn, 'mat-custom-page');
+    // newIndex = this._curPageObj.pageIndex + this._showTotalPages;
     switch (i) {
       case pageIndex:
         this.ren.setAttribute(linkBtn, 'disabled', 'disabled');
         break;
       case this._pageGapTxt:
-        let newIndex = this._curPageObj.pageIndex + this._showTotalPages;
-
-        if (newIndex >= this.numOfPages) { newIndex = this.lastPageIndex; }
-
-        if (pageIndex !== this.lastPageIndex) {
-          this.ren.listen(linkBtn, 'click', () => {
-            console.log('working: ', pageIndex);
-            this.switchPage(newIndex);
-          });
-        }
-
-        if (pageIndex === this.lastPageIndex) {
-          // If end reached remove the last dotted element
-          this.ren.setStyle(linkBtn, 'display', 'none');
-        }
+        this.ren.listen(linkBtn, 'click', () => {
+          this.switchPage(this._currentPage + this._showTotalPages);
+        });
+        break;
+      case this._pageGapTxt2:
+        this.ren.listen(linkBtn, 'click', () => {
+          this.switchPage(this._currentPage - this.showTotalPages);
+        });
         break;
       default:
         this.ren.listen(linkBtn, 'click', () => {
@@ -218,67 +197,21 @@ export class PaginationDirective {
   }
   // calculates the button range based on class input parameters and based on current page index value.
   // Used to render new buttons after event.
+
   private initPageRange(): void {
-    const middleIndex = (this._rangeStart + this._rangeEnd) / 2;
-
-    this._rangeStart = this.calcRangeStart(middleIndex);
-    this._rangeEnd = this.calcRangeEnd(middleIndex);
-
+    console.log(this._rangeStart, this._rangeEnd);
+    this._rangeStart = this._currentPage - this._showTotalPages / 2;
+    this._rangeEnd = this._currentPage + this._showTotalPages / 2;
+    console.log(this._rangeStart, this._rangeEnd);
     this.buildPageNumbers();
   }
 
-  // Helper function To calculate start of button range
-  private calcRangeStart(middleIndex: number): number {
-    switch (true) {
-      case this._curPageObj.pageIndex === 0 && this._rangeStart !== 0:
-        return 0;
-      case this._curPageObj.pageIndex > this._rangeEnd:
-        return this._curPageObj.pageIndex + this.inc > this.lastPageIndex
-          ? this.lastPageIndex - this.inc * 2
-          : this._curPageObj.pageIndex - this.inc;
-      case this._curPageObj.pageIndex > this._curPageObj.previousPageIndex &&
-        this._curPageObj.pageIndex > middleIndex &&
-        this._rangeEnd < this.lastPageIndex:
-        return this._rangeStart + 1;
-      case this._curPageObj.pageIndex < this._curPageObj.previousPageIndex &&
-        this._curPageObj.pageIndex < middleIndex &&
-        this._rangeStart > 0:
-        return this._rangeStart - 1;
-      default:
-        return this._rangeStart;
-    }
-  }
-  // Helpter function to calculate end of button range
-  private calcRangeEnd(middleIndex: number): number {
-    switch (true) {
-      case this._curPageObj.pageIndex === 0 &&
-        this._rangeEnd !== this._showTotalPages:
-        return this._showTotalPages - 1;
-      case this._curPageObj.pageIndex > this._rangeEnd:
-        return this._curPageObj.pageIndex + this.inc > this.lastPageIndex
-          ? this.lastPageIndex
-          : this._curPageObj.pageIndex + 1;
-      case this._curPageObj.pageIndex > this._curPageObj.previousPageIndex &&
-        this._curPageObj.pageIndex > middleIndex &&
-        this._rangeEnd < this.lastPageIndex:
-        return this._rangeEnd + 1;
-      case this._curPageObj.pageIndex < this._curPageObj.previousPageIndex &&
-        this._curPageObj.pageIndex < middleIndex &&
-        this._rangeStart >= 0 &&
-        this._rangeEnd > this._showTotalPages - 1:
-        return this._rangeEnd - 1;
-      default:
-        return this._rangeEnd;
-    }
-  }
-  // Helper function to switch page on non first, last, next and previous buttons only.
   private switchPage(i: number): void {
-    console.log('switch', i);
-    const previousPageIndex = this.matPag.pageIndex;
+    this._currentPage = i;
     this.matPag.pageIndex = i;
-    this.matPag['_emitPageEvent'](previousPageIndex);
     this.initPageRange();
   }
+
   // Initialize default state after view init
   public ngAfterViewInit() {
     this._rangeStart = 0;
